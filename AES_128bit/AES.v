f4e30a0bf297a7716a3b563a00f20c2ab769a38b
宁国霖（Ning Guolin） <2271551064@qq.com>
2025-07-20 18:27:07 +0800

    在AES模块里完成了理论上的各模块逻辑调度，还在等待验证

--- AES_128bit/AES.v
+++ AES_128bit/AES.v
@@ -32,35 +32,66 @@ module AES(
 //传入的128位明文其实就已经是状态矩阵1了，但是要先把它写成矩阵的形式
 //二维数组不好赋值，先把它当成一维编写
 reg [1:128] aes_reg; //寄存器记录明文到密文的寄存器
+reg [1:128] key_reg; //寄存器记录密钥到轮密钥的寄存器
 wire [1:8] statusMatrix [1:16];
 wire [1:128] ByteTransOut; //字节代换的输出
 wire [1:128] aesregsignal; //寄存器的输出信号
+wire [1:128] keyregsignal; //轮密钥的输出信号
+wire [1:128] keyupdatesignal; //轮密钥的输出信号
+reg [3:0] roundcount; //轮计数器
 
+wire [1:128] rowshiftOut; //行移位的输出
+wire [1:128] MixColumnOut; //列混合的输出
 
-
-genvar i;
-generate
-    for (i = 1;i < 17 ;i=i+1) begin
-    assign statusMatrix[i] = aes_reg[8*i-7:8*i];
-end
-endgenerate
-assign aesregsignal = aes_reg; //将寄存器的输出信号赋值给aesOut
+assign aesregsignal = aes_reg; //将寄存器的输出信号赋值给wire
+assign keyregsignal = key_reg; //将轮密钥的输出信号赋值给wire
     always @(posedge clk or negedge rst_n)           
         begin                                        
-            if(!rst_n)                               
+            if(!rst_n) begin                              
                 aes_reg <= 128'b0; //复位时清空寄存器
+                key_reg <= 128'b0; //复位时清空密钥寄存器
+                roundcount <= 4'b0; //复位时清空轮计数器
+            end
             else if(start) begin
                 aes_reg <= aesIn; //开始时将输入的明文赋值给寄存器
+                key_reg <= keyIn; //将输入的密钥赋值给轮密钥寄存器
+                roundcount <= 4'b0; //轮计数器归零
             end
-            else if(ready) begin //如果ready信号为高，表示处理完成
-                aes_reg <= aesOut; //将输出的密文赋值给寄存器
-            end                                                                    
+            else if (roundcount==4'b0) begin
+                aes_reg <= aesregsignal^keyregsignal; //轮密钥加
+                roundcount <= roundcount + 1; //轮计数器加1
+                //补充key的更新逻辑
+                key_reg <= keyupdatesignal; //更新轮密钥
+            end
+            else if(roundcount<4'b1010) begin
+                aes_reg <= MixColumnOut^keyupdatesignal; //轮密钥加
+                roundcount <= roundcount + 1; //轮计数器加1
+                key_reg <= keyupdatesignal; //更新轮密钥
+            end    
+            else if(roundcount==4'b1010) begin
+                aes_reg <= MixColumnOut^keyupdatesignal; //轮密钥加
+                roundcount <= 4'b1011; //轮计数器归零
+            end
+                
         end                                          
+assign ready = (roundcount==4'b1011)?1:0; //当轮计数器为1011时，表示AES加密完成
 //进行字节代换
 Byte_transform Byte_transform_inst (
     .aesIn(aesregsignal), //输入的128位明文
     .ByteTransOut(ByteTransOut) //输出的128位代换后的结果
 );
-
+rowshift rowshift_inst (
+    .aesIn(ByteTransOut), //输入的128位代换后的结果
+    .rowshiftOut(rowshiftOut) //输出的128位行移位后的结果
+);
+column_mix column_mix_inst (
+    .messagein(rowshiftOut), //输入的128位行移位后的结果
+    .messageout(MixColumnOut) //输出的128位列混合后的结果
+);
+RoundKeyGen RoundKeyGen_inst (
+    .keyin(keyregsignal), //输入的轮密钥
+    .round(roundcount), //当前轮数
+    .nextkey(keyupdatesignal+4'b0001) //输出的下一轮密钥
+);
                                                                    
 endmodule
